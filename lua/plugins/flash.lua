4a536c97055ba056c750d05f6dfca9b9db1f82e4
shirann <shirann@qq.com>
2023-08-30 05:31:55 +0000

    change: disable flash search

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -1,1 +1,16 @@
-require('flash').setup()
+require('flash').setup({
+  labels = "asdfghjklqwertyuiopzxcvbnm",
+  search = {
+    -- search/jump in all windows
+    multi_window = true,
+  },
+  modes = {
+    -- options used when flash is activated through
+    -- a regular search with `/` or `?`
+    search = {
+      -- when `true`, flash will be activated during regular search by default.
+      -- You can always toggle when searching with `require("flash").toggle()`
+      enabled = true,
+    }
+  }
+})
