7a1a3cc64ed777f13d11a568c237e6d9e7a86582
ShIRann <shirann@qq.com>
2023-09-05 23:42:30 +0800

    fix: disable search and multi-window feat

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -2,7 +2,7 @@ require('flash').setup({
   labels = "asdfghjklqwertyuiopzxcvbnm",
   search = {
     -- search/jump in all windows
-    multi_window = true,
+    multi_window = false,
   },
   modes = {
     -- options used when flash is activated through
@@ -10,7 +10,7 @@ require('flash').setup({
     search = {
       -- when `true`, flash will be activated during regular search by default.
       -- You can always toggle when searching with `require("flash").toggle()`
-      enabled = true,
+      enabled = false,
     }
   }
 })
