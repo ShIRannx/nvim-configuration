08332fb1ab1f3b30cc324384114fbf40ff539fd0
ShIRann <shirann@qq.com>
2023-09-14 09:38:18 +0800

    change: @macro event to lualine, message to virtualtext

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,8 +1,18 @@
+local noice = require('noice')
 require('lualine').setup {
   options = {
     icons_enabled = false,
     theme = 'auto',
     component_separators = '|',
     section_separators = '',
+  },
+  sections = {
+    lualine_x = {
+      {
+        color = { fg = "#d4d4d4" },
+        noice.api.statusline.mode.get,
+        cond = noice.api.statusline.mode.has,
+      }
+    },
   },
 }
--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -10,17 +10,17 @@ require("noice").setup({
       ["cmp.entry.get_documentation"] = true,
     },
   },
-  routes = {
-    {
-      filter = {
-        event = "notify",
-        find = "",
-      },
-      opts = { skip = true },
-    },
-  },
+  routes = {},
   messages = {
-    enabled = false, -- enables the Noice messages UI
+    enabled = true, -- enables the Noice messages UI
+    -- NOTE: If you enable messages, then the cmdline is enabled automatically.
+    -- This is a current Neovim limitation.
+    view = "mini", -- default view for messages
+    view_error = "mini", -- view for errors
+    view_warn = "mini", -- view for warnings
+    view_history = "messages", -- view for :messages
+    view_search = "virtualtext", -- view for search count messages. Set to `false` to disable
+
   },
   -- you can enable a preset for easier configuration
   presets = {
