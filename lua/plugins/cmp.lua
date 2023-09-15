3642c8cb9a782329738be8b012afef0f84142aa1
ShIRann <shirann@qq.com>
2023-09-16 00:42:35 +0800

    change: make tab more functional

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -27,7 +27,7 @@ end
 cmp.setup({
     snippet = {
         expand = function(args)
-            require('luasnip').lsp_expand(args.body)
+            luasnip.lsp_expand(args.body)
         end,
     },
     sorting = {
@@ -48,41 +48,29 @@ cmp.setup({
         },
     },
     mapping = cmp.mapping.preset.insert({
-        ['<C-b>'] = cmp.mapping.scroll_docs( -4),
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<C-e>'] = cmp.mapping.abort(), -- 取消补全，esc也可以退出
         ['<CR>'] = cmp.mapping.confirm({ select = true }),
-        ['<Tab>'] = cmp.mapping.confirm({ select = true }),
 
-        -- ["<Tab>"] = cmp.mapping(function(fallback)
-        --   if cmp.visible() then
-        --     cmp.select_next_item()
-        --   elseif luasnip.expandable() then
-        --     luasnip.expand()
-        --   elseif luasnip.expand_or_jumpable() then
-        --     luasnip.expand_or_jump()
-        --   elseif check_backspace() then
-        --     fallback()
-        --   else
-        --     fallback()
-        --   end
-        -- end, {
-        --   "i",
-        --   "s",
-        -- }),
-        --
-        -- ["<S-Tab>"] = cmp.mapping(function(fallback)
-        --   if cmp.visible() then
-        --     cmp.select_prev_item()
-        --   elseif luasnip.jumpable(-1) then
-        --     luasnip.jump(-1)
-        --   else
-        --     fallback()
-        --   end
-        -- end, {
-        --   "i",
-        --   "s",
-        -- }),
+        ["<Tab>"] = cmp.mapping(function(fallback)
+            if luasnip.expandable() then
+                luasnip.expand()
+            elseif luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
+            elseif check_backspace() then
+                fallback()
+            else
+                cmp.confirm({ select = true })
+            end
+        end, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+            else
+                fallback()
+            end
+        end, { "i", "s" }),
     }),
     -- 这里重要
     sources = cmp.config.sources({
