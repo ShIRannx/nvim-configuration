b2e337654c53b1031ccc01cc3d47d13cee62b779
ShIRann <shirann@qq.com>
2023-09-16 00:49:59 +0800

    add: new lang; change: format

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,7 +1,20 @@
 require('nvim-treesitter.configs').setup {
     -- 添加不同语言
-    ensure_installed = { "vim", "bash", "c", "cpp", "javascript", "json", "lua", "python", "typescript", "tsx",
-        "css", "rust","yaml" },                                                                                                                 -- one of "all" or a list of languages
+    ensure_installed = {
+        "bash",
+        "javascript",
+        "json",
+        "lua",
+        "python",
+        "typescript",
+        "tsx",
+        "html",
+        "sql",
+        "go",
+        "css",
+        "rust",
+        "yaml"
+    },
 
     highlight = { enable = true },
     -- indent = { enable = true },
