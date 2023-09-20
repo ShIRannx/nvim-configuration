808c7179ba1660966bca9c025dd639b9d35ce499
ShIRann <shirann@qq.com>
2023-09-20 11:55:06 +0800

    change: reverse mismodify

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -3,7 +3,7 @@ null_ls.setup({
     sources = {
         -- null_ls.builtins.formatting.buf,
         -- null_ls.builtins.formatting.xmlformat,
-        -- null_ls.builtins.formatting.prettier.with({ extra_args = { '--single-quote', '--arrow-parens', 'avoid' } }),
+        null_ls.builtins.formatting.prettier.with({ extra_args = { '--single-quote', '--arrow-parens', 'avoid' } }),
         -- null_ls.builtins.formatting.black.with({ extra_args = { "--fast", '--line-length', 79 } }),
         -- null_ls.builtins.formatting.isort,
         --null_ls.builtins.completion.spell,
