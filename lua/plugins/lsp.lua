6eeb200271bdcfda6591748cfa9e48698f1634ac
ShIRann <shirann@qq.com>
2024-01-22 12:41:01 +0800

    change: revert null-ls for diagnostic

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -128,4 +128,17 @@ return {
         "williamboman/mason-lspconfig.nvim",
         config = config
     },
+    {
+        "nvimtools/none-ls.nvim",
+        opts = function()
+            local null_ls = require("null-ls")
+            return {
+                sources = {
+                    null_ls.builtins.diagnostics.ruff,
+                },
+            }
+        end,
+        ft = { "python" },
+        event = "VeryLazy",
+    }
 }
