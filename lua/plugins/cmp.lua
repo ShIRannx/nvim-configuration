f2db9cd8ca67d7552f1d639373e170b884850acd
ShIRann <shirann@qq.com>
2024-01-27 20:45:53 +0800

    cmp ghost supported

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,38 +1,44 @@
-local config = function() 
+local config = function()
     local cmp = require("cmp")
     local luasnip = require("luasnip")
     local defaults = require("cmp.config.default")()
     local ts_utils = require("nvim-treesitter.ts_utils")
     local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+    vim.api.nvim_set_hl(0, "CmpGhostText", { link = "LineNr", default = true })
 
     local ts_node_func_parens_disabled = {
-      -- ecma
-      named_imports = true,
-      -- rust
-      use_declaration = true,
+        -- ecma
+        named_imports = true,
+        -- rust
+        use_declaration = true,
     }
 
     local default_handler = cmp_autopairs.filetypes["*"]["("].handler
     cmp_autopairs.filetypes["*"]["("].handler = function(char, item, bufnr, rules, commit_character)
-      local node_type = ts_utils.get_node_at_cursor():type()
-      if ts_node_func_parens_disabled[node_type] then
-        if item.data then
-          item.data.funcParensDisabled = true
-        else
-          char = ""
+        local node_type = ts_utils.get_node_at_cursor():type()
+        if ts_node_func_parens_disabled[node_type] then
+            if item.data then
+                item.data.funcParensDisabled = true
+            else
+                char = ""
+            end
         end
-      end
-      default_handler(char, item, bufnr, rules, commit_character)
+        default_handler(char, item, bufnr, rules, commit_character)
     end
 
     cmp.event:on(
-      "confirm_done",
-      cmp_autopairs.on_confirm_done({
-        sh = false,
-      })
+        "confirm_done",
+        cmp_autopairs.on_confirm_done({
+            sh = false,
+        })
     )
 
     return {
+        experimental = {
+            ghost_text = {
+                hl_group = "CmpGhostText",
+            },
+        },
         completion = { completeopt = "menu,menuone,noinsert" },
         snippet = {
             expand = function(args)
