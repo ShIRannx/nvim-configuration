7059af0b8b308dacb5457e3febcbca50a067cdc5
ShIRann <shirann@qq.com>
2024-02-12 21:46:59 +0800

    colorscheme onedark

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "navarasu/onedark.nvim",
+    opts = {
+      style = "warmer",
+      highlights = {
+        illuminatedWord = { bg = "$bg2" },
+        illuminatedCurWord = { bg = "$bg2" },
+        IlluminatedWordText = { bg = "$bg2" },
+        IlluminatedWordRead = { bg = "$bg2" },
+        IlluminatedWordWrite = { bg = "$bg2" },
+      },
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "onedark",
+    },
+  },
+}
