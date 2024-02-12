35e17ec40922edecf78010f8c2b0ef138d3865fd
ShIRann <shirann@qq.com>
2024-02-12 22:35:28 +0800

    add: completion for sql

--- lua/plugins/extras/lang/sql.lua
+++ lua/plugins/extras/lang/sql.lua
@@ -6,4 +6,19 @@ return {
       vim.list_extend(opts.ensure_installed, { "sql" })
     end,
   },
+  {
+    "nvim-cmp",
+    ft = { "sql", "mysql", "plsql" },
+    dependencies = { "kristijanhusak/vim-dadbod-completion" },
+    build = function()
+      vim.api.nvim_create_autocmd("FileType", {
+        desc = "dadbod completion",
+        group = vim.api.nvim_create_augroup("dadbod_cmp", { clear = true }),
+        pattern = { "sql", "mysql", "plsql" },
+        callback = function()
+          require("cmp").setup.buffer({ sources = { { name = "vim-dadbod-completion" } } })
+        end,
+      })
+    end,
+  },
 }
