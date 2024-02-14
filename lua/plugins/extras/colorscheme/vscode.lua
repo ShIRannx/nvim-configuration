80d16e6162d17de67e8aaa9aecedbd2df0dce9eb
ShIRann <shirann@qq.com>
2024-02-14 17:26:14 +0800

    fix: pretty path color; change: disable tab icon

--- lua/plugins/extras/colorscheme/vscode.lua
+++ lua/plugins/extras/colorscheme/vscode.lua
@@ -53,7 +53,11 @@ return {
   },
   {
     "bufferline.nvim",
+    optional = true,
     opts = {
+      options = {
+        show_close_icon = false,
+      },
       highlights = {
         fill = {
           fg = { attribute = "fg", highlight = "Normal" },
@@ -221,5 +225,29 @@ return {
         },
       },
     },
+  },
+  {
+    "lualine.nvim",
+    optional = true,
+    opts = function(_, opts)
+      local Util = require("lazyvim.util")
+      local icons = require("lazyvim.config").icons
+
+      opts.sections.lualine_c = {
+        Util.lualine.root_dir(),
+        {
+          "diagnostics",
+          symbols = {
+            error = icons.diagnostics.Error,
+            warn = icons.diagnostics.Warn,
+            info = icons.diagnostics.Info,
+            hint = icons.diagnostics.Hint,
+          },
+        },
+        { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+        { Util.lualine.pretty_path(), color = Util.ui.fg("Normal") },
+      }
+      return opts
+    end,
   },
 }
