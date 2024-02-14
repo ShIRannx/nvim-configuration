27a579ca7852f41e9a8c90355cd3b9bf7bae7075
ShIRann <shirann@qq.com>
2024-02-14 14:57:01 +0800

    change: indicator color corrected and remove dead config

--- lua/plugins/extras/colorscheme/vscode.lua
+++ lua/plugins/extras/colorscheme/vscode.lua
@@ -32,6 +32,15 @@ return {
           MiniIndentscopePrefix = { nocombine = true },
           MiniIndentscopeSymboloff = { link = "IndentBlanklineChar" },
           MiniIndentscopeSymbol = { link = "IndentBlanklineContextChar" },
+
+          illuminate = { bg = "#484848" },
+          illuminatedWord = { link = "illuminate" },
+          illuminatedCurWord = { link = "illuminate" },
+          IlluminatedWordText = { link = "illuminate" },
+          IlluminatedWordRead = { link = "illuminate" },
+          IlluminatedWordWrite = { link = "illuminate" },
+
+          BufferLineIndicatorSelected = { link = "BufferCurrentSign" },
         },
       }
     end,
@@ -45,20 +54,6 @@ return {
   {
     "bufferline.nvim",
     opts = {
-      options = {
-        close_command = "bdelete %d",
-        indicator = {
-          style = "icon",
-          icon = " ",
-        },
-        left_trunc_marker = "",
-        modified_icon = "●",
-        offsets = { { filetype = "NvimTree", text = "EXPLORER", text_align = "center" } },
-        right_mouse_command = "bdelete! %d",
-        right_trunc_marker = "",
-        show_close_icon = false,
-        show_tab_indicators = true,
-      },
       highlights = {
         fill = {
           fg = { attribute = "fg", highlight = "Normal" },
