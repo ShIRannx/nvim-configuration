1bc7d4c456c36f0c60a80c036b7012393ce011e0
ShIRann <shirann@qq.com>
2024-02-16 13:06:03 +0800

    change: kb for visual-multi

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,4 +1,9 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.g.VM_maps = { ["I BS"] = "<C-h>" }
+vim.g.VM_maps = {
+  ["I BS"] = "<C-h>",
+  ["Exit"] = "<C-c>",
+  ["Select Cursor Up"] = "<S-k>",
+  ["Select Cursor Down"] = "<S-j>",
+}
