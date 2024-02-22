29e3682a6da8a0ee6087b6ee5c9664effd134e48
ShIRann <shirann@qq.com>
2024-02-22 13:54:41 +0800

    change: remove unnecessary ansible to yaml register

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,9 +1,6 @@
 return {
   {
     "nvim-treesitter",
-    config = function()
-      vim.treesitter.language.register("yaml", "yaml.ansible")
-    end,
     opts = {
       textobjects = {
         select = {
