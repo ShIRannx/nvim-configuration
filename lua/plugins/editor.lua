67c5fdb9b24d9b05e8681f5860de5f8e59dbbab6
ShIRann <shirann@qq.com>
2024-03-04 17:44:46 +0800

    fix: neo-tree overwritten opts

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,7 +1,7 @@
 return {
   {
     "neo-tree.nvim",
-    opts = function()
+    opts = function(_, opts)
       local renderer = require("neo-tree.ui.renderer")
       local getSiblings = function(state, node)
         local parent = state.tree:get_node(node:get_parent_id())
@@ -9,43 +9,41 @@ return {
         return siblings
       end
 
-      return {
-        window = { position = "right" },
-        filesystem = {
-          window = {
-            mappings = {
-              ["oc"] = "noop",
-              ["od"] = "noop",
-              ["og"] = "noop",
-              ["om"] = "noop",
-              ["on"] = "noop",
-              ["os"] = "noop",
-              ["ot"] = "noop",
-              ["o"] = { command = "open", nowait = true },
-              ["O"] = { "show_help", nowait = false, config = { title = "Order by", prefix_key = "O" } },
-              ["Oc"] = { "order_by_created", nowait = false },
-              ["Od"] = { "order_by_diagnostics", nowait = false },
-              ["Og"] = { "order_by_git_status", nowait = false },
-              ["Om"] = { "order_by_modified", nowait = false },
-              ["On"] = { "order_by_name", nowait = false },
-              ["Os"] = { "order_by_size", nowait = false },
-              ["Ot"] = { "order_by_type", nowait = false },
+      opts.window = vim.tbl_extend("force", opts.window or {}, {
+        position = "right",
+        mappings = {
+          ["oc"] = "noop",
+          ["od"] = "noop",
+          ["og"] = "noop",
+          ["om"] = "noop",
+          ["on"] = "noop",
+          ["os"] = "noop",
+          ["ot"] = "noop",
+          ["o"] = { command = "open", nowait = true },
+          ["O"] = { "show_help", nowait = false, config = { title = "Order by", prefix_key = "O" } },
+          ["Oc"] = { "order_by_created", nowait = false },
+          ["Od"] = { "order_by_diagnostics", nowait = false },
+          ["Og"] = { "order_by_git_status", nowait = false },
+          ["Om"] = { "order_by_modified", nowait = false },
+          ["On"] = { "order_by_name", nowait = false },
+          ["Os"] = { "order_by_size", nowait = false },
+          ["Ot"] = { "order_by_type", nowait = false },
 
-              -- sibling
-              ["K"] = function(state)
-                local node = state.tree:get_node()
-                local siblings = getSiblings(state, node)
-                renderer.focus_node(state, siblings[1])
-              end,
-              ["J"] = function(state)
-                local node = state.tree:get_node()
-                local siblings = getSiblings(state, node)
-                renderer.focus_node(state, siblings[#siblings])
-              end,
-            },
-          },
+          -- sibling
+          ["K"] = function(state)
+            local node = state.tree:get_node()
+            local siblings = getSiblings(state, node)
+            renderer.focus_node(state, siblings[1])
+          end,
+          ["J"] = function(state)
+            local node = state.tree:get_node()
+            local siblings = getSiblings(state, node)
+            renderer.focus_node(state, siblings[#siblings])
+          end,
         },
-      }
+      })
+
+      return opts
     end,
   },
   {
