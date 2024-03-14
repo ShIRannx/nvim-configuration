4fa0fbe2bbfd5d84bfb4ee0c5c26f6857f3a0dd9
ShIRann <shirann@qq.com>
2024-03-14 11:28:47 +0800

    change: use official dadbod-ui configuration

--- lua/plugins/extras/lang/sql.lua
+++ lua/plugins/extras/lang/sql.lua
@@ -7,18 +7,15 @@ return {
     end,
   },
   {
-    "nvim-cmp",
-    ft = { "sql", "mysql", "plsql" },
-    dependencies = { "kristijanhusak/vim-dadbod-completion" },
-    build = function()
-      vim.api.nvim_create_autocmd("FileType", {
-        desc = "dadbod completion",
-        group = vim.api.nvim_create_augroup("dadbod_cmp", { clear = true }),
-        pattern = { "sql", "mysql", "plsql" },
-        callback = function()
-          require("cmp").setup.buffer({ sources = { { name = "vim-dadbod-completion" } } })
-        end,
-      })
+    "kristijanhusak/vim-dadbod-ui",
+    dependencies = {
+      { "tpope/vim-dadbod", lazy = true },
+      { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
+    },
+    cmd = { "DBUI", "DBUIToggle", "DBUIAddConnection", "DBUIFindBuffer" },
+    init = function()
+      -- Your DBUI configuration
+      vim.g.db_ui_use_nerd_fonts = 1
     end,
   },
 }
