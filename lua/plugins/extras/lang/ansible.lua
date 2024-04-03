c14c81a529a254609de028df60f2224bdf245696
ShIRann <shirann@qq.com>
2024-04-04 01:15:06 +0800

    change: enable copilot for ansible file

--- lua/plugins/extras/lang/ansible.lua
+++ lua/plugins/extras/lang/ansible.lua
@@ -14,4 +14,13 @@ return {
       vim.list_extend(opts.ensure_installed, { "ansible-lint" })
     end,
   },
+  {
+    "copilot.lua",
+    ft = "yaml.ansible",
+    optional = true,
+    opts = function(_, opts)
+      opts.filetypes = vim.tbl_extend("force", opts.filetypes or {}, { ["yaml.ansible"] = true })
+      return opts
+    end,
+  },
 }
