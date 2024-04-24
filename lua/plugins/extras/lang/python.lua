6b21c89fd752875a79580dc6b2df00c4349f4716
ShIRann <shirann@qq.com>
2024-04-24 22:12:13 +0800

    change: fit the lazyvim version v10.22.0

--- lua/plugins/extras/lang/python.lua
+++ lua/plugins/extras/lang/python.lua
@@ -1,23 +1,5 @@
 return {
   {
-    "nvim-lspconfig",
-    opts = {
-      servers = { ruff_lsp = { mason = false } },
-      setup = {
-        ruff_lsp = function()
-          return true
-        end,
-      },
-    },
-  },
-  {
-    "mason.nvim",
-    opts = function(_, opts)
-      opts.ensure_installed = opts.ensure_installed or {}
-      vim.list_extend(opts.ensure_installed, { "ruff" })
-    end,
-  },
-  {
     "mfussenegger/nvim-lint",
     opts = {
       linters_by_ft = {
