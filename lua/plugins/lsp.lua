07314bccf4bff99fcbb95f0da3ade5d4397420d5
shenzijian <shenzijian@cyou-inc.com>
2025-11-07 10:53:54 +0800

    Update: drop deprecated; regular lazyvim update

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,11 +1,16 @@
 return {
   {
     "nvim-lspconfig",
-    init = function()
-      local keys = require("lazyvim.plugins.lsp.keymaps").get()
-      keys[#keys + 1] = { "K", false }
-      keys[#keys + 1] = { "gh", vim.lsp.buf.hover }
-      keys[#keys + 1] = { "gk", vim.lsp.buf.signature_help }
-    end,
+    opts = {
+      servers = {
+        ["*"] = {
+          keys = {
+            { "K", false },
+            { "gh", vim.lsp.buf.hover },
+            { "gk", vim.lsp.buf.signature_help },
+          },
+        },
+      },
+    },
   },
 }
