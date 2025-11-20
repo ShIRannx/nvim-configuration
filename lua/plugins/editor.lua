970e4353dd4bedce47b17dcb19b6875384060bc9
shenzijian <shenzijian@cyou-inc.com>
2025-11-20 17:30:58 +0800

    Update: jump snippet with []

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -85,19 +85,11 @@ return {
     version = "*",
     opts = {
       keymap = {
-        ["<Tab>"] = {
-          function(cmp)
-            if cmp.snippet_active() then
-              return cmp.accept()
-            else
-              return cmp.select_and_accept()
-            end
-          end,
-          "snippet_forward",
-          "fallback",
-        },
-        ["<S-Tab>"] = { "snippet_backward", "fallback" },
+        ["<Tab>"] = { "accept", "fallback" },
+        ["<S-Tab>"] = { "select_prev", "fallback" },
 
+        ["]"] = { "snippet_forward", "fallback" },
+        ["["] = { "snippet_backward", "fallback" },
         ["<Up>"] = { "select_prev", "fallback" },
         ["<Down>"] = { "select_next", "fallback" },
         ["<C-p>"] = { "select_prev", "fallback" },
